2309d93d998dec5b2c0ebfe886653359c70f6d15
KingaB <tcx7@o2.pl>
2023-02-23 13:54:34 +0100

    Add options.json and default_options.json, saving options, reseting options to default, confirmation screen

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -1,19 +1,24 @@
 import pygame
+import json
 
 pygame.init()
 pygame.display.init()
 pygame.font.init()
 
 #OPTIONS
-BOARD_SIZE = 3 # check if >= CONNECTED_TO_WIN needed
+BOARD_SIZE = 3
+MAX_BOARD_SIZE = 25
 CONNECTED_TO_WIN = 3
 WIDTH, HEIGHT = 800, 600
 IS_TURN_TEXT = True
 TURN_TEXT_FONT = pygame.font.SysFont('Calibri', 60)
-WINNER_TEXT_FONT = pygame.font.SysFont('comicsnas', 60)
+WINNER_TEXT_FONT = pygame.font.SysFont('Calibri', 60)
 MENU_FONT = pygame.font.SysFont('verdana', 75, bold = pygame.font.Font.bold)
 MENU_OPTIONS_FONT = pygame.font.SysFont('verdana', 60)
+OPTIONS_OPTIONS_FONT = pygame.font.SysFont('verdana', 40)
 HOVERED_MENU_OPTIONS_FONT = pygame.font.SysFont('verdana', 65, bold = pygame.font.Font.bold)
+HOVERED_OPTIONS_OPTIONS_FONT = pygame.font.SysFont('verdana', 45, bold = pygame.font.Font.bold)
+CONFIRMATION_FONT = pygame.font.SysFont('verdana', 50, bold = pygame.font.Font.bold)
 TURN_BOARDER_WIDTH = 20
 DIS_TEXT_TO_BOARDER = 5
 BOARD_WIDTH, BOARD_HEIGHT = WIDTH - 2*TURN_BOARDER_WIDTH, HEIGHT - TURN_TEXT_FONT.get_height() - 3*TURN_BOARDER_WIDTH + DIS_TEXT_TO_BOARDER*2
@@ -31,10 +36,11 @@ PLAYER_2_COLOR = (0, 200, 100)
 DRAW_COLOR = MENU_COLOR = (0, 200, 200)
 HOVERED_MENU_COLOR = (0, 230, 230)
 MENU_OPTIONS = ("Single Player", "Player vs Player", "Options", "Exit")
-OPTIONS_OPTIONS = ("Player 1 name", "Player 2 name", "Back to menu")
+OPTIONS_OPTIONS = ("Player 1 name", "Player 2 name", "Board size",  "Defaults", "Back to menu")
 LETTER_DISTANCE = 50
 OK_COLOR = (30, 230, 30)
 NO_OK_COLOR = (230, 30, 30)
+BACKGROUND_CONFIRMATION_COLOR = (50, 50, 50)
 
 P1_NAME = "Player 1"
 P2_NAME = "Player 2"
@@ -45,9 +51,74 @@ FIELD_HEIGHT, FIELD_WIDTH = (BOARD_HEIGHT - (BOARD_SIZE - 1) * BOARDER_WIDTH)//B
 ICON_SIZE = min(FIELD_HEIGHT, FIELD_WIDTH)//2
 
 # To-do
-# save options to file
+# number input
 # test for bugs
 # add single player
+
+def confirmation():
+    WIN.fill(BACKGROUND_COLOR)
+    pygame.draw.rect(WIN, BACKGROUND_CONFIRMATION_COLOR, (WIDTH//4, HEIGHT//4, WIDTH//2, HEIGHT//2))
+    text = CONFIRMATION_FONT.render("Are you sure?", True, MENU_COLOR)
+    WIN.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//4 + 10))
+
+    ok_button = pygame.Rect(WIDTH//2 + 40, HEIGHT//2 + 30, WIDTH//6, HEIGHT//8)
+    pygame.draw.rect(WIN, OK_COLOR, ok_button)
+    ok_text = CONFIRMATION_FONT.render("YES", True, BACKGROUND_CONFIRMATION_COLOR)
+    WIN.blit(ok_text, (ok_button.x + ok_button.width//2 - ok_text.get_width()//2, ok_button.y + ok_button.height//2 - ok_text.get_height()//2))
+
+    no_button = pygame.Rect(WIDTH//2 - 40 - WIDTH//6, HEIGHT//2 + 30, WIDTH//6, HEIGHT//8)
+    pygame.draw.rect(WIN, NO_OK_COLOR, no_button)
+    no_text = CONFIRMATION_FONT.render("NO", True, BACKGROUND_CONFIRMATION_COLOR)
+    WIN.blit(no_text, (no_button.x + no_button.width // 2 - no_text.get_width() // 2,
+                       no_button.y + no_button.height // 2 - no_text.get_height() // 2))
+
+    pygame.display.update()
+
+    while True:
+        clock.tick(FPS)
+        handle_events()
+        if pygame.mouse.get_pressed()[0]:
+            if pygame.Rect.collidepoint(ok_button, pygame.mouse.get_pos()):
+                return True
+            if pygame.Rect.collidepoint(no_button, pygame.mouse.get_pos()):
+                return False
+
+def recalculate():
+    global FIELD_HEIGHT, FIELD_WIDTH, ICON_SIZE, WINNING_LINE_WIDTH
+    FIELD_HEIGHT, FIELD_WIDTH = (BOARD_HEIGHT - (BOARD_SIZE - 1) * BOARDER_WIDTH) // BOARD_SIZE, (
+                BOARD_WIDTH - (BOARD_SIZE - 1) * BOARDER_WIDTH) // BOARD_SIZE
+    ICON_SIZE = min(FIELD_HEIGHT, FIELD_WIDTH) // 2
+
+    if BOARD_SIZE >= 10:
+        WINNING_LINE_WIDTH = 2*BOARDER_WIDTH
+
+def set_default_options():
+    with open("options.json", "w") as options:
+        with open("default_options.json", "r") as default:
+            options.write(default.read())
+
+def load_options():
+    global Player1_name, Player2_name, BOARD_SIZE
+
+    with open("options.json", "r") as file:
+        dict_json = json.loads(file.read())
+        if "Player 1 name" in dict_json:
+            Player1_name = dict_json["Player 1 name"].upper()
+        if "Player 2 name" in dict_json:
+            Player2_name = dict_json["Player 2 name"].upper()
+        if "Board size" in dict_json:
+            new_size = dict_json["Board size"]
+            if new_size >= 3 and new_size <= MAX_BOARD_SIZE and new_size >= CONNECTED_TO_WIN:
+                BOARD_SIZE = new_size
+
+def save_options():
+    dict_json = None
+    with open("options.json", "r") as file:
+        dict_json = json.loads(file.read())
+        dict_json["Player 1 name"] = Player1_name
+        dict_json["Player 2 name"] = Player2_name
+    with open("options.json", "w") as file:
+        json.dump(dict_json, file)
 
 def draw_letter(x, y, letter, size):
     letter_font = pygame.font.SysFont('Calibri', size)
@@ -62,6 +133,7 @@ def valid_name(name):
         elif letter == "_" and result != "":
             return result
     return result
+
 def text_input(x = -1, y = -1, size = 60, no_of_letters = 8):
     x = WIDTH//2 - no_of_letters * (10+LETTER_DISTANCE) //2
     y = HEIGHT // 2 - size
@@ -134,10 +206,19 @@ def options_option_action(no):
     if no == 0:
         global Player1_name
         Player1_name = text_input()
+        save_options()
     elif no == 1:
         global Player2_name
         Player2_name = text_input()
+        save_options()
     elif no == 2:
+        pass
+    elif no == 3:
+        if confirmation():
+            set_default_options()
+            load_options()
+            recalculate()
+    elif no == 4:
         main()
 def draw_options():
     pygame.Surface.fill(WIN, BACKGROUND_COLOR)
@@ -147,16 +228,16 @@ def draw_options():
     pygame.draw.rect(WIN, MENU_COLOR, dec)
     pygame.draw.rect(WIN, MENU_COLOR, pygame.Rect.move(dec, header.get_width() // 2 + WIDTH // 2, 0))
 
-    distance = (HEIGHT - 4 * TURN_BOARDER_WIDTH - MENU_FONT.get_height()) // len(MENU_OPTIONS)
+    distance = (HEIGHT - 4 * TURN_BOARDER_WIDTH - MENU_FONT.get_height()) // len(OPTIONS_OPTIONS)
     options_positiones = []
     no_option = 0
     x = 3 * TURN_BOARDER_WIDTH + MENU_FONT.get_height()
     for option in OPTIONS_OPTIONS:
-        text = MENU_OPTIONS_FONT.render(option, True, MENU_COLOR)
+        text = OPTIONS_OPTIONS_FONT.render(option, True, MENU_COLOR)
         options_positiones.append(
             pygame.Rect(WIDTH // 2 - text.get_width() // 2, x, text.get_width(), text.get_height()))
         if pygame.Rect.collidepoint(options_positiones[no_option], pygame.mouse.get_pos()):
-            text = HOVERED_MENU_OPTIONS_FONT.render(option, True, HOVERED_MENU_COLOR)
+            text = HOVERED_OPTIONS_OPTIONS_FONT.render(option, True, HOVERED_MENU_COLOR)
             WIN.blit(text, (WIDTH // 2 - text.get_width() // 2, x))
         else:
             WIN.blit(text, (WIDTH // 2 - text.get_width() // 2, x))
@@ -362,12 +443,12 @@ def check_win(num, x, y, board):
             in_row += 1
         else:
             if in_row >= max_in_row:
-                mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, x, i
+                mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, x, i - 1
                 max_in_row = in_row
             in_row = 0
 
     if in_row >= max_in_row:
-        mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, x, i
+        mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, beg_x, BOARD_SIZE - 1
         max_in_row = in_row
     if max_in_row >= CONNECTED_TO_WIN:
         return mbeg_x, mbeg_y, mend_x, mend_y
@@ -382,7 +463,7 @@ def check_win(num, x, y, board):
             in_col += 1
         else:
             if in_col >= max_in_col:
-                mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, i, y
+                mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, i - 1, y
                 max_in_col = in_col
             in_col = 0
 
@@ -410,7 +491,7 @@ def check_win(num, x, y, board):
         else:
             if l_diag >= max_l_diag:
                 max_l_diag = l_diag
-                mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, diag_x, diag_y
+                mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, diag_x - 1, diag_y - 1
             l_diag = 0
         
         diag_x += 1
@@ -439,7 +520,7 @@ def check_win(num, x, y, board):
             r_diag += 1
         else:
             if r_diag >= max_r_diag:
-                mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, diag_x, diag_y
+                mbeg_x, mbeg_y, mend_x, mend_y = beg_x, beg_y, diag_x - 1, diag_y - 1
                 max_r_diag = r_diag
             r_diag = 0
         
@@ -515,6 +596,8 @@ def sp():
     pass
 
 def main():
+    load_options()
+    recalculate()
     while True:
         clock.tick(FPS)
         draw_menu()
